b5257d1381aaa27cafd23d338bb920ca4bb0f83b
francisco.tln <francisco.tln@outlook.com>
2018-06-17 21:37:59 +0200

    rewrite using loop function instead of passing the source as talkback. Update devDeps

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 const isNumber = n => n != null && !isNaN(n) && isFinite(n) && n.constructor === Number;
 
+const checkArgs = (from, to, step) => {
+  if (step === 0) {
+    throw new Error('Step cannot be zero');
+  }
+  if (!isNumber(from) || !isNumber(to) || !isNumber(step)) {
+    throw new Error('Arguments must be numbers');
+  }
+};
+
 const range = (from, to, step = 1) => {
-  if (step === 0) throw 'Step cannot be zero';
-  if (!isNumber(from) || !isNumber(to) || !isNumber(step)) throw 'Arguments must be numbers';
-  let sent = from - step;
-  let sink;
-  let end;
-  const source = (t, d) => {
-    if (end) return;
-    if (t === 0) {
-      sink = d;
-      sink(0, source);
-    }
-    if (t === 1) {
-      sent += step;
-      if ((to > from && sent <= to) || (to < from && sent >= to)) {
-        sink(1, sent);
-      } else if (!end) {
-        sink(2);
-        end = true;
+  checkArgs(from, to, step);
+  return (start, sink) => {
+    if (start !== 0) return;
+    let sent = from - step;
+    let ended = false;
+    let inLoop = false;
+    const loop = () => {
+      inLoop = true;
+      while (got1 && !ended) {
+        got1 = false;
+        sent += step;
+        if ((to > from && sent <= to) || (to < from && sent >= to)) {
+          sink(1, sent);
+        } else {
+          ended = true;
+          sink(2);
+        }
       }
-    }
+      inLoop = false;
+    };
+
+    sink(0, t => {
+      if (ended) return;
+      if (t === 1) {
+        got1 = true;
+        if (!inLoop) loop();
+      }
+      if (t === 2) {
+        ended = true;
+      }
+    });
   };
-  return source;
 };
 
 module.exports = range;
